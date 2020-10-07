e085965f9b2d5c8024bd242bd338711a062f5c88
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2020-10-08 02:04:32 +0900

    アノテーションが間違ってたので修正

--- app/src/main/java/com/example/dagger/hilt/MainViewModel.kt
+++ app/src/main/java/com/example/dagger/hilt/MainViewModel.kt
@@ -1,14 +1,14 @@
 package com.example.dagger.hilt
 
 import androidx.hilt.Assisted
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.dagger.hilt.di.qualifier.HttpClientForApi
 import com.example.dagger.hilt.di.qualifier.HttpClientForOtherSite
 import okhttp3.OkHttpClient
-import javax.inject.Inject
 
-class MainViewModel @Inject constructor(
+class MainViewModel @ViewModelInject constructor(
     @HttpClientForApi private val okHttpClient: OkHttpClient,
     @HttpClientForOtherSite private val okHttpClient2: OkHttpClient,
     private val repository: FooDataSource,
